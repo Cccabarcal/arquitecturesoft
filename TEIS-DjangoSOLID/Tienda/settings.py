11bf944b65c489e2483d5638ac777f726a3e6a97
Cristian <cccabarcal@eafit.edu.co>
2026-04-03 15:32:59 -0500

    commit

--- TEIS-DjangoSOLID/Tienda/settings.py
+++ TEIS-DjangoSOLID/Tienda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-6to$5m=6=aoi5e#omz-$c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,100.31.109.121,*').split(',')
 
 
 # Application definition
